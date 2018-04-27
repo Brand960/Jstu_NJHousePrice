fecdca60306a242f5033d93744c7c24c0b093cd4
Yue <yue414369311@gmail.com>
2018-04-27 04:22:09 +0000

    latest version

--- cron.sh
+++ cron.sh
@@ -6,6 +6,6 @@ cd /home/NJ_HousePriceCrawler/log
 now=`date +%F`
 clean=`mongo test --eval "db.data.remove({})"`
 # scrapy=`nohup /usr/local/bin/scrapy crawl Lianjia >> /home/NJ_HousePriceCrawler/log/$now.log 2>&1 &`
-/usr/local/bin/scrapy crawl Lianjia > /home/NJ_HousePriceCrawler/log/$now.log
+/usr/local/bin/scrapy crawl Lianjia
 mongoexport -d test -c data -csv -f price,name,lng,lat,location,when -o /home/NJ_HousePriceCrawler/csv/$now.csv
 
